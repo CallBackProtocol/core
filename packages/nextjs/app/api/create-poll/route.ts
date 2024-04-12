d26e90f4d616132896203fd13b3dae453abaf46a
Yash Goyal <yashgo0018@gmail.com>
2024-04-13 01:04:31 +0200

    fixed a  bug

--- packages/nextjs/app/api/create-poll/route.ts
+++ packages/nextjs/app/api/create-poll/route.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
-import OpenAI from "openai";
 import BlandRequest from "~~/Model/BlandRequest";
 import Poll from "~~/Model/Poll";
 import connectDB from "~~/connectDB";
