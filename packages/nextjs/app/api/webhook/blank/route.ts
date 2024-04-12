e2a8914696f0466d5d14d0ea9f582e0c8abb1baa
Yash Goyal <yashgo0018@gmail.com>
2024-04-13 00:42:46 +0200

    fixed a bug

--- packages/nextjs/app/api/webhook/blank/route.ts
+++ packages/nextjs/app/api/webhook/blank/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
-import axios from "axios";
 import OpenAI from "openai";
-import BlandRequest from "~~/Model/BlandRequest";
-import Poll from "~~/Model/Poll";
 import connectDB from "~~/connectDB";
 
 export async function POST(req: Request) {
